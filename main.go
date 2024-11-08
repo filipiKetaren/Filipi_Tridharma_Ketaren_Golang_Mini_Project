779b273cb4dc318e240c04ec309e38b2e25b684f
Filipi Tridharma Ketaren <158836873+filipiKetaren@users.noreply.github.com>
2024-11-08 09:37:52 +0700

    sembunyi env dan dsn config

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"log"
 	"miniproject/config"
 	authController "miniproject/controller/auth"
 	authRepo "miniproject/repo/auth"
 	authService "miniproject/service/auth"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	loadEnv()
 	db, _ := config.ConnectDatabase()
 	config.MigrateDB(db)
 	e := echo.New()
@@ -19,4 +22,12 @@ func main() {
 
 	e.POST("/login", authController.LoginController)
 	e.Start(":8000")
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		panic("failed load env")
+	}
 }
